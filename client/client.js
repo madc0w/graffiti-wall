35b3d0ed94abec8644c0b55e27b23eda15da9d06
Marc A. Donis <mad7@runbox.com>
2019-04-20 17:38:05 +0200

    .

--- client/client.js
+++ client/client.js
@@ -1,7 +1,21 @@
+emoji = require("node-emoji");
+
 wallId = new ReactiveVar("general");
+emojis = new ReactiveVar([]);
 
 Meteor.startup(function() {
 	console.log("client started");
+
+	const _emojis = [];
+	const keys = Object.keys(emoji.emoji).sort();
+	for (key of keys) {
+		_emojis.push({
+			key : key,
+			emoji : emoji.emoji[key]
+		});
+	//
+	}
+	emojis.set(_emojis);
 
 	Tracker.autorun(function() {
 		Meteor.subscribe("wall", wallId.get());
--- client/js/main.js
+++ client/js/main.js
@@ -1,13 +1,25 @@
 //import { Template } from "meteor/templating";
 //import { ReactiveVar } from "meteor/reactive-var";
+
 const fadeDuration = 1200;
 const toastDuration = 4000;
 const flightTime = 12000;
+const flightTimeVariation = 3000;
+const numMessages = 2;
 
 const isPostInProgress = new ReactiveVar(false);
 const toastText = new ReactiveVar();
+const messages = new ReactiveVar({});
 
 Template.main.helpers({
+	emojis : function() {
+		return emojis.get();
+	},
+
+	smiley : function() {
+		return emoji.emojify(":smiley:");
+	},
+
 	wall : function() {
 		const wall = Walls.findOne({
 			id : wallId.get()
@@ -16,9 +28,7 @@ Template.main.helpers({
 	},
 
 	messages : function() {
-		return Messages.find({
-			wallId : wallId.get()
-		});
+		return Object.values(messages.get());
 	},
 
 	isPostInProgress : function() {
@@ -74,10 +84,53 @@ Template.main.events({
 			});
 		}
 	},
+
+	"keyup #add-post-input" : function(e) {
+		const text = $("#add-post-input").val();
+		const el = $("#add-post-input")[0];
+		const cursorPos = getCaretPosition(el);
+		const emojified = emojify(text);
+		$("#add-post-input").val(emojified);
+		setCaretPosition(el, cursorPos - (text.length - emojified.length));
+	},
+
+	"click #emoji-button" : function(e) {
+		$("#emojis").toggle();
+	},
+
+	"click .emoji-selection" : function(e) {
+		const text = $("#add-post-input").val();
+		const el = $("#add-post-input")[0];
+		const cursorPos = getCaretPosition(el);
+		const newText = text.substring(0, cursorPos) + emoji.get(this.key) + text.substring(cursorPos);
+		$("#add-post-input").val(newText);
+		setCaretPosition(el, cursorPos + 1);
+	},
+});
+
+Template.main.onCreated(function() {
+	Meteor.setInterval(() => {
+		const _messages = messages.get();
+		if (Object.keys(_messages).length < numMessages) {
+			const message = selectMessage();
+			if (message) {
+				_messages[message._id] = message;
+				messages.set(_messages);
+			}
+		}
+	}, 400);
+});
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+Template.message.helpers({
+	emojify : emojify,
 });
 
 Template.message.onRendered(function() {
-	const element = $("#" + this.data._id);
+	const id = this.data._id;
+	const element = $("#" + id);
 	const init = {};
 	const final = {};
 	if (Math.random() < 0.5) {
@@ -110,10 +163,15 @@ Template.message.onRendered(function() {
 	element.animate({
 		left : final.x + "px",
 		top : final.y + "px"
-	}, flightTime, null, function() {
+	}, flightTime + ((Math.random() - 0.5) * flightTimeVariation), null, function() {
 		element.remove();
+		const _messages = messages.get();
+		delete _messages[id];
+		messages.set(_messages);
 	});
 });
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 function toast(textKey) {
 	toastText.set(TAPi18n.__(textKey));
@@ -125,4 +183,65 @@ function toast(textKey) {
 	Meteor.setTimeout(function() {
 		toastText.set(null);
 	}, toastDuration);
+}
+
+function selectMessage() {
+	const allMessages = Messages.find({
+		wallId : wallId.get()
+	}).fetch();
+	if (allMessages.length > 0) {
+		return allMessages[Math.floor(Math.random() * allMessages.length)];
+	}
+	return null;
+}
+
+function emojify(text) {
+	text = text.replace(":)", ":smiley:");
+	text = text.replace(":D", ":grin:");
+	return emoji.emojify(text);
+}
+
+
+/*
+** Returns the caret (cursor) position of the specified text field (oField).
+** Return value range is 0-oField.value.length.
+*  from https://stackoverflow.com/questions/2897155/get-cursor-position-in-characters-within-a-text-input-field
+*/
+function getCaretPosition(oField) {
+	var iCaretPos;
+
+	// IE Support
+	if (document.selection) {
+
+		// Set focus on the element
+		oField.focus();
+
+		// To get cursor position, get empty selection range
+		var oSel = document.selection.createRange();
+
+		// Move selection start to 0 position
+		oSel.moveStart("character", -oField.value.length);
+
+		// The caret position is selection length
+		iCaretPos = oSel.text.length;
+	} else if (oField.selectionStart || oField.selectionStart == "0") {
+		// Firefox support
+		iCaretPos = oField.selectionDirection == "backward" ? oField.selectionStart : oField.selectionEnd;
+	}
+	// Return results
+	return iCaretPos;
+}
+
+// https://stackoverflow.com/questions/512528/set-keyboard-caret-position-in-html-textbox
+function setCaretPosition(elem, caretPos) {
+	if (elem.createTextRange) {
+		const range = elem.createTextRange();
+		range.move("character", caretPos);
+		range.select();
+	} else if (elem.selectionStart) {
+		elem.focus();
+		elem.setSelectionRange(caretPos, caretPos);
+	} else {
+		elem.focus();
+	}
 }
