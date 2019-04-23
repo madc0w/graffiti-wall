3d275d3d7ea62d9b5c7d826621ca391a61e4e811
Marc A. Donis <marc.a.donis@primeresults.lu>
2019-04-23 17:33:23 +0200

    .

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -7,9 +7,9 @@ rm -r ~/wallator-build/
 export METEOR_ALLOW_SUPERUSER=true
 meteor build --verbose ~/wallator-build --server=http://www.wallator.com
 
-cd ~/wallator-build/android/project/build/outputs/apk/release
+cd ~mad/git/graffiti-wall/.meteor/local/cordova-build/platforms/android/app/build/outputs/apk/release
 
-jarsigner -verbose -sigalg SHA1withRSA -tsa http://timestamp.digicert.com -digestalg SHA1 android-release-unsigned.apk wallator
+jarsigner -verbose -sigalg SHA1withRSA -tsa http://timestamp.digicert.com -digestalg SHA1 app-release-unsigned.apk wallator
 
-$ANDROID_HOME/build-tools/23.0.1/zipalign 4 android-release-unsigned.apk signed.apk
+$ANDROID_HOME/build-tools/23.0.1/zipalign 4 app-release-unsigned.apk signed.apk
 
