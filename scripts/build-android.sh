3720b6d39c0bc88f3c319c114612857e85bcf851
Marc A. Donis <mad7@runbox.com>
2019-04-23 16:11:30 +0200

    .

---
+++ scripts/build-android.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git pull
+
+rm -r ~/analogizer-build/
+
+meteor build --verbose ~/wallator-build --server=http://www.wallator.com
+
+cd ~/wallator-build/android/project/build/outputs/apk/release
+
+jarsigner -verbose -sigalg SHA1withRSA -tsa http://timestamp.digicert.com -digestalg SHA1 android-release-unsigned.apk wallator
+
+$ANDROID_HOME/build-tools/23.0.1/zipalign 4 android-release-unsigned.apk signed.apk
+
