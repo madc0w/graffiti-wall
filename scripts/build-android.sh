0845c917b0b0b2916780f5d0d2f30025e2ffaa7a
Marc A. Donis <marc.a.donis@primeresults.lu>
2019-04-23 18:55:40 +0200

    .

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -8,6 +8,7 @@ export METEOR_ALLOW_SUPERUSER=true
 meteor build --verbose ~/wallator-build --server=http://www.wallator.com
 
 cd ~mad/git/graffiti-wall/.meteor/local/cordova-build/platforms/android/app/build/outputs/apk/release
+rm signed.apk
 
 jarsigner -verbose -sigalg SHA1withRSA -tsa http://timestamp.digicert.com -digestalg SHA1 app-release-unsigned.apk wallator
 
