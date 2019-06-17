ef3f29a4ff583c43e3e523c5cc5a0d562dae3d4f
Marc A. Donis <marc.a.donis@primeresults.lu>
2019-06-17 18:00:26 +0200

    .

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -10,6 +10,7 @@ meteor build --verbose ~/wallator-build --server=http://www.wallator.com
 cd ~mad/git/graffiti-wall/.meteor/local/cordova-build/platforms/android/app/build/outputs/apk/release
 rm signed.apk
 
+# keytool -genkey -alias distrochess -keyalg RSA -keysize 2048 -validity 10000
 jarsigner -verbose -sigalg SHA1withRSA -tsa http://timestamp.digicert.com -digestalg SHA1 app-release-unsigned.apk wallator
 
 $ANDROID_HOME/build-tools/23.0.1/zipalign 4 app-release-unsigned.apk signed.apk
